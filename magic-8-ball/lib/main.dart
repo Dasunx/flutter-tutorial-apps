16db8f08c52943b0b6b88f8f6b510d48307fd881
unknown <mwdasun@gmail.com>
2020-09-11 22:59:06 +0530

    MagicBall added

---
+++ magic-8-ball/lib/main.dart
@@ -0,0 +1,51 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        body: MagicBall(),
+      ),
+    ),
+  );
+}
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballState = 1;
+  void changeBall() {
+    setState(() {
+      ballState = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeBall();
+              },
+              child: Image.asset('images/ball$ballState.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+    ;
+  }
+}
